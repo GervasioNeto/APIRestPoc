10b545073f285574b2b2475cfa9e91663682b8ea
Gervásio Neto <gervasiolopesn@gmail.com>
2026-05-01 23:00:33 -0300

    chore: added anon patient method (#9)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO consultorio (numero) VALUES (1);
+INSERT INTO consultorio (numero) VALUES (2);
+INSERT INTO consultorio (numero) VALUES (3);
+INSERT INTO consultorio (numero) VALUES (4);
+INSERT INTO consultorio (numero) VALUES (5);
